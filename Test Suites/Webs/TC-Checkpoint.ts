d7e8318d731d403883a919110575d103f92e58aa
kuxuluvu <vuduc762@gmail.com>
2024-06-12 21:01:43 +0700

    add checkpoint tc

---
+++ Test Suites/Webs/TC-Checkpoint.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC-Checkpoint</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>567e5aff-1fb0-4e51-99e0-f96d4ddc0b29</testSuiteGuid>
+   <testCaseLink>
+      <guid>4ca254e0-4a2b-418b-b40b-c49c8f45aa64</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Webs/TC-Checkpoint</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
